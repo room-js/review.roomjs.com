46b80d089858b1725a9c6b301c8198b1c2d70166
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-03 22:11:18 +0000

    Deploy review.roomjs.com to github.com/room-js/review.roomjs.com.git:gh-pages

---
+++ precache-manifest.daca084b001055555e89e90c171e960d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c7b45b41d5679f97b75a",
+    "url": "/js/chunk-vendors.0aaa1742.js"
+  },
+  {
+    "revision": "440673a94c994bd16886",
+    "url": "/js/app.30818992.js"
+  },
+  {
+    "revision": "04c7e93f123294934460",
+    "url": "/js/about.6cfc2462.js"
+  },
+  {
+    "revision": "194437f96a81af37fbb162bd98eaa338",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "440673a94c994bd16886",
+    "url": "/css/app.4a848d41.css"
+  }
+];
